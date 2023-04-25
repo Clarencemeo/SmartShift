00a603a08eb2edd2d4d71849d45fbca11194d2c5
Cielo-Lionflower <73498918+Cielo-Lionflower@users.noreply.github.com>
2023-04-25 10:07:48 -0700

    Changed apperance/style of break time change modal

--- components/BreakTimerInput.js
+++ components/BreakTimerInput.js
@@ -1,12 +1,12 @@
 import { useState } from "react";
-import { StyleSheet, View, Modal, TextInput, Button, Text} from "react-native";
+import { StyleSheet, View, Modal, TextInput, Button, Text, Pressable} from "react-native";
 
 function BreakTimerInput(props) {
     return (
         <Modal visible = {props.visible} animationType = "slide" >
             <View style={styles.inputContainer}>
-                <View>
-                    <Text>Change Break Time</Text>
+                <View style={styles.inputTop}>
+                    <Text style= {styles.changeTitle}>Change Break Time</Text>
                     <TextInput
                         inputMode = 'numeric'
                         keyboardType = "number-pad" 
@@ -14,12 +14,22 @@ function BreakTimerInput(props) {
                         placeholder="5"
                         // onChangeText = {workTimerInputHandler}
                         // value = {enteredWorkTimerInput}
-                        // style = {styles.numberInput}
+                        style = {styles.numberInput}
                     />
                 </View>
                 <View style = {styles.buttonStyle}>
-                    <Button title = "Cancel" onPress = {props.onCancel}/>
-                    <Button title = "Change"/>
+                    <Pressable>
+                        <View>
+                            <Text style= {styles.timerText}>Change</Text>
+                        </View>
+                    </Pressable>
+                    <Pressable onPress={props.onCancel}>
+                        <View>
+                            <Text style= {styles.timerText}>Cancel</Text>
+                        </View>
+                    </Pressable>
+                    {/* <Button title = "Cancel" onPress = {props.onCancel}/> */}
+                    {/* <Button title = "Change"/> */}
                 </View>
             </View>
         </Modal>
@@ -32,31 +42,69 @@ export default BreakTimerInput;
 const styles = StyleSheet.create({
     inputContainer: {
         flex: 1, 
-        flexDirection: 'row',
+        flexDirection: 'column',
         justifyContent: 'space-between',
         alignItems: 'center',
         marginBottom: 24,
         marginBottomWidth: 1, 
+        backgroundColor: '#fbc4ab'
         // borderBottomColor: 
     },
-    textInput: {
-        borderWidth: 1,
-        width: '70%',
-        marginRight: 8, 
-        padding: 8,
-    }, 
+    inputTop: {
+        flex: 1,
+        alignItems: 'center',
+        marginTop: 90,
+
+    },
     numberInput: {
         height: 50,
         width: 75,
-        fontSize: 32,
+        fontSize: 36,
         borderBottomColor: "black",
         borderBottomWidth: 2,
         color: "black",
         marginVertical: 8,
-        textAlign: 'center'
+        textAlign: 'center',
+        justifyContent: 'center'
+    },
+    changeTitle: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 10,
+        marginBottom: 30,
+        fontSize: 26,
+        // borderRadius: 30,
+        padding: 10,
+        backgroundColor: '#f4978e',
+        borderWidth: 5,
+        borderColor: '#f08080',
+        //backgroundColor: 'red',
+        fontWeight: 'bold',
+        opacity: 0.8,
+        overflow: 'hidden'
+    },
+    timerText: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        margin: 10,
+        fontSize: 26,
+        borderRadius: 30,
+        padding: 10,
+        backgroundColor: '#f4978e',
+        borderWidth: 5,
+        borderColor: '#f08080',
+        //backgroundColor: 'red',
+        fontWeight: 'bold',
+        opacity: 0.8,
+        overflow: 'hidden'
     },
     buttonStyle: {
-        // flex: 1,
-        // flexDirection: 'row'
+        alignItems: 'top',
+        // justifyContent: 'top',
+        flex: 1,
+        flexDirection: 'row',
+        fontSize: 46,
+        fontWeight: 'bold',
+        marginBottom: 300,
     }
 });
