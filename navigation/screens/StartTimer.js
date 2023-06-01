111d8caf4c4df6fd8a5d192e199d3e43ab087068
Clarence Ortega <cleorteg@ucsc.edu>
2023-05-31 18:23:08 -0700

    fixed merge issues

--- navigation/screens/StartTimer.js
+++ navigation/screens/StartTimer.js
@@ -152,8 +152,8 @@ export default function TimerApp({ route }) {
   function reflectHandler() {
     navigation.navigate("Reflect", {
       numOfSlices: slices,
-      workDuration: route.params.workTimerDuration,
-      breakDuration: route.params.breakTimerDuration,
+      workDuration: workDuration,
+      breakDuration: breakDuration,
     });
   }
 
